f250d04561298b13fce18b7ffe9dc326dc2d19d7
Bryce Kerley <bkerley@brycekerley.net>
2008-10-19 15:30:47 -0400

    plays nice with monome

--- icicle.rb
+++ icicle.rb
@@ -1,18 +1,25 @@
 require 'lib/archaeopteryx'
 require '/Users/bkerley/Documents/ruby-monome/monome/lib/monome.rb'
-clock = Clock.new(30)
-midi = LiveMIDI.new(:clock => clock, # confusion!!!!!!!!!!
-                     :logging => false,
-                     :midi_destination => 0)
 
-class Clock < Monome::Application
+
+class Icicle < Monome::Application
 	every 0.5,	:metronome
 	
 	on :initialize do
-		
+		@midi = LiveMIDI.new(:clock => Clock.new(30), # confusion!!!!!!!!!!
+		                     :logging => false,
+		                     :midi_destination => 0)
 	end
 	
 	on :metronome do
-		midi.play(Note.new(0, 69, 0.25, 100))
+		@midi.play(Note.new(0, 69, 0.25, 100))
+	end
+	
+	on :press do |row, column, state|
+		stop unless state == 1
+		note = (row * 8) + column + 48
+		@midi.play(Note.new(0, note, 0.25, 100))
 	end
 end
+
+Icicle.run(:device => Monome::M40h.new)
